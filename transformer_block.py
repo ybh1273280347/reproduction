7d6b516b011f3873a1f0fe72b1ad14e52e9db920
ybh1273280347 <ybh1273280347@163.com>
2026-03-22 12:47:00 +0800

    Update transformer_block.py

--- transformer_block.py
+++ transformer_block.py
@@ -2,6 +2,7 @@ import torch
 import torch.nn as nn
 import math
 from dataclasses import dataclass
+from einops import rearrange, repeat
 
 @dataclass
 class TransformerBlockConfig:
@@ -25,7 +26,7 @@ def scaled_dot_attention(Q, K, V, mask=None):
     return attention
 
 
-class MultiHeadAttention(nn.Module):
+class MultiHeadAttentionForStudy(nn.Module):
     def __init__(self, config):
         super().__init__()
         assert config.d_model % config.n_heads == 0
@@ -57,6 +58,41 @@ class MultiHeadAttention(nn.Module):
         ], dim=-1))
 
 
+class MutiHeadAttention(nn.Module):
+    def __init__(self, config):
+        super().__init__()
+        assert config.d_model % config.n_heads == 0
+        self.d_model: int = config.d_model
+        self.n_heads: int = config.n_heads
+        self.d_k: int = config.d_model // config.n_heads
+
+        self.W_Q = nn.Linear(self.d_model, self.d_model, bias=False)
+        self.W_K = nn.Linear(self.d_model, self.d_model, bias=False)
+        self.W_V = nn.Linear(self.d_model, self.d_model, bias=False)
+        self.W_O = nn.Linear(self.d_model, self.d_model, bias=False)
+
+    def forward(self, Q, K=None, V=None, mask=None):
+        if K is None and V is None:
+            K = V = Q
+
+        # b -> batch_size, s -> seq_len, h -> n_heads, d -> d_k, (h d) -> d_model
+        Q = rearrange(self.W_Q(Q), 'b s (h d) -> b h s d', h=self.n_heads)
+        K = rearrange(self.W_K(K), 'b s (h d) -> b h s d', h=self.n_heads)
+        V = rearrange(self.W_V(V), 'b s (h d) -> b h s d', h=self.n_heads)
+
+        B = Q.shape[0]
+
+        if mask is not None:
+            if len(mask.shape) == 2:
+                mask = mask.unsqueeze(0).unsqueeze(0)
+            elif len(mask.shape) == 3:
+                mask = mask.unsqueeze(1)
+
+        attention = scaled_dot_attention(Q, K, V, mask=mask)
+        attention = rearrange(attention, 'b h s d -> b s (h d)')
+
+        return self.W_O(attention)
+
 class EncoderBlock(nn.Module):
     def __init__(self, config):
         super().__init__()
@@ -73,11 +109,9 @@ class EncoderBlock(nn.Module):
         self.layer_norm2 = nn.LayerNorm(config.d_model)
 
     def forward(self, X, mask=None):
-        attn = self.self_attention(X, mask=mask)
-        X = self.layer_norm1(attn) + X
+        X = X + self.self_attention(self.layer_norm1(X), mask=mask)
 
-        feed_forward = self.feed_forward(X)
-        X = self.layer_norm2(feed_forward) + X
+        X = X + self.feed_forward(self.layer_norm2(X))
 
         return X
 
@@ -101,17 +135,17 @@ class DecoderBlock(nn.Module):
         self.layer_norm3 = nn.LayerNorm(config.d_model)
 
 
-    def forward(self, X, to_cross_attn=None, mask=None):
+def forward(self, X, to_cross_attn=None, self_mask=None, cross_mask=None):
 
-        masked_attn = self.masked_self_attention(X, mask=mask)
-        X = self.layer_norm1(masked_attn) + X
+    masked_attn = self.masked_self_attention(self.layer_norm1(X), mask=self_mask)
+    X = X + masked_attn
 
-        if to_cross_attn is not None:
-            cross_attn = self.cross_attention(Q=X, K=to_cross_attn, V=to_cross_attn)
-            X = self.layer_norm2(cross_attn) + X
+    if to_cross_attn is not None:
+        cross_attn = self.cross_attention(Q=self.layer_norm2(X), K=to_cross_attn, V=to_cross_attn, mask=cross_mask)
+        X = X + cross_attn
 
-        feed_forward = self.feed_forward(X)
-        X = self.layer_norm3(feed_forward) + X
+    feed_forward = self.feed_forward(self.layer_norm3(X))
+    X = X + feed_forward
 
-        return X
+    return X
 
